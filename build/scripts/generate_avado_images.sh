d830411b28913ee38fce9447c83051e3a10480f5
stefaan <stefaan@ponnet.com>
2020-05-03 09:11:23 +0200

    update version & build process

--- build/scripts/generate_avado_images.sh
+++ build/scripts/generate_avado_images.sh
@@ -3,6 +3,9 @@
 source ./.dappnode_profile
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
+echo "-----------------------------------------------------------------------------"
+figlet "DNP ADMIN"
+echo "-----------------------------------------------------------------------------"
 
 echo "Cloning & building DNP_ADMIN..."
 if [ -f "/images/admin.dnp.dappnode.eth_${ADMIN_VERSION}.tar.xz" ]; then
@@ -18,6 +21,10 @@ else
     cp ./DNP_ADMIN/docker-compose.yml /images/docker-compose-admin.yml
 fi
 
+echo "-----------------------------------------------------------------------------"
+figlet "DNP DAPPMANAGER"
+echo "-----------------------------------------------------------------------------"
+
 echo "Cloning & building DNP_DAPPMANAGER..."
 if [ -f "/images/dappmanager.dnp.dappnode.eth_${DAPPMANAGER_VERSION}.tar.xz" ]; then
     echo "target file already exist"
@@ -28,6 +35,10 @@ else
     docker save dappmanager.dnp.dappnode.eth:${DAPPMANAGER_VERSION} | xz -e9vT0 > /images/dappmanager.dnp.dappnode.eth_${DAPPMANAGER_VERSION}.tar.xz
     cp ./DNP_DAPPMANAGER/docker-compose.yml /images/docker-compose-dappmanager.yml
 fi
+
+echo "-----------------------------------------------------------------------------"
+figlet "DNP WIFI"
+echo "-----------------------------------------------------------------------------"
 
 echo "Cloning & building DNP_WIFI..."
 if [ -f "/images/wifi.dnp.dappnode.eth_${WIFI_VERSION}.tar.xz" ]; then
@@ -40,6 +51,12 @@ else
     cp ./DNP_WIFI/docker-compose.yml /images/docker-compose-wifi.yml
     cp ./DNP_WIFI/wifi.dnp.dappnode.eth.env /images/wifi.dnp.dappnode.eth.env
 fi
+
+
+echo "-----------------------------------------------------------------------------"
+figlet "DNP BIND"
+echo "-----------------------------------------------------------------------------"
+
 
 echo "Cloning & building DNP_BIND..."
 if [ -f "/images/bind.dnp.dappnode.eth_${BIND_VERSION}.tar.xz" ]; then
@@ -66,5 +83,6 @@ echo "These are the images that are copied to dappnode/CORE"
 ls -l dappnode/CORE
 
 
-
-echo "Finished!"
+echo "-----------------------------------------------------------------------------"
+figlet "Finished"
+echo "-----------------------------------------------------------------------------"
